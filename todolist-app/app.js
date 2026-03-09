bd390856eec59200850ee233673d349bd032e053
Kerem Döleksöz <keremdoleksoz58@gmail.com>
2026-03-09 11:45:41 +0300

    Quick Task mechanism and tasks are added

--- todolist-app/app.js
+++ todolist-app/app.js
@@ -1,10 +1,11 @@
 document.addEventListener(`DOMContentLoaded`, () => {
     defaultCategory();
     categoryOperations();
+    taskOperations();
 })
 
 
-
+// ****************************************** CATEGORIES ******************************************
 function categoryOperations() {
     listCategories();
 
@@ -31,7 +32,7 @@ function categoryOperations() {
 }
 
 function addCategory(categoryName) {
-    
+
     const value = categoryName.trim();
 
     if (value === "") return;
@@ -50,10 +51,23 @@ function addCategory(categoryName) {
 }
 
 function editCategory(categoryId, newCategoryName) {
+    const value = newCategoryName.trim();
+    if (value === "") return;
 
+    const categories = JSON.parse(localStorage.getItem(("Categories") || "[]"));
+
+    for (let i = 0; i < categories.length; i++) {
+        if (categories[i].id == categoryId) {
+            categories[i].name = value;
+        }
+    }
+
+    localStorage.setItem("Categories", JSON.stringify(categories));
+    listCategories();
 }
 
-function deleteCategory(categoryId) {
+function deleteCategory(categoryId) { // UNFINISHED
+    const categories = JSON.parse(localStorage.getItem(("Categories") || "[]"));
 
 }
 
@@ -76,6 +90,19 @@ function listCategories() {
             const deleteButton = document.createElement(`button`);
             deleteButton.textContent = `Delete`;
 
+            editButton.addEventListener(`click`, () => {
+                const newName = prompt("Add new category name", categories[i].name);
+                if (newName != null) {
+                    editCategory(categories[i].id, newName);
+                }
+
+            });
+
+            deleteButton.addEventListener('click', () => {
+                deleteCategory(categories[i].id);
+            });
+
+
             li.appendChild(span);
             li.appendChild(editButton);
             li.appendChild(deleteButton);
@@ -97,5 +124,78 @@ function defaultCategory() {
         });
 
         localStorage.setItem(`Categories`, JSON.stringify(categories));
+    }
+}
+
+// ****************************************** TASKS ******************************************
+
+function taskOperations(){
+    listTask();
+    const addQuickTaskButton = document.getElementById("addQuickTask");
+
+    addQuickTaskButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        addQuickTask();
+    })
+}
+
+function addQuickTask() {
+    const taskInput = document.getElementById("taskInput");
+
+        const value = taskInput.value.trim();
+        if(value == "") return;
+
+        const tasks = JSON.parse(localStorage.getItem("Tasks") || "[]");
+
+        const newTask = {
+            id: crypto.randomUUID(),
+            name: value,
+            category: "default",
+            startDate: null,
+            endDate: null,
+            createdAt: new Date().toISOString(),
+            priority: null,
+            completed: false,
+        }
+
+        tasks.push(newTask);
+        localStorage.setItem("Tasks", JSON.stringify(tasks));
+        listTask();
+
+        taskInput.value = "";
+    
+}
+
+
+function listTask(){
+    const tasks = JSON.parse(localStorage.getItem("Tasks") || "[]");
+    const taskList = document.getElementById("taskList");
+
+    taskList.innerHTML = "";
+
+    if(tasks.length > 0){
+        for(let i=0; i<tasks.length; i++){
+            const li = document.createElement("li");
+
+            const checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+
+            const span = document.createElement("span");
+            span.textContent = tasks[i].name;
+
+            const editButton = document.createElement("button");
+            editButton.textContent = "Edit";
+
+            const deleteButton = document.createElement("button");
+            deleteButton.textContent = "Delete";
+
+            li.appendChild(checkbox);
+            li.appendChild(span);
+            li.appendChild(editButton);
+            li.appendChild(deleteButton);
+
+            taskList.appendChild(li);
+           
+        }
     }
 }
